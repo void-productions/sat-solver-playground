fb1942ee893a05cb7989d1c87980bd60fdf9023e
memoryleak47 <memoryleak47@protonmail.ch>
2024-08-17 23:40:47 +0200

    impl sudoku base rules

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -27,16 +27,74 @@ fn parse_sudoku(s: &str) -> Sudoku {
     }).collect()
 }
 
+// exactly one of these variables should be true.
+fn push_one_of(vars: Vec<Var>, smap: &mut SymbolMap, c: &mut ClauseSet) {
+    let disj = vars.iter().map(|x| (*x, true)).collect();
+    c.insert(disj);
+
+    for x in &vars {
+        for y in &vars {
+            if x != y {
+                let mut clause = Clause::new();
+                clause.insert((*x, false));
+                clause.insert((*y, false));
+                c.insert(clause);
+            }
+        }
+    }
+}
+
+fn index_sets() -> Vec<BTreeSet<(u8, u8)>> {
+    let mut out = Vec::new();
+
+    // rows:
+    for x in 1..=9 {
+        let set = (1..=9).map(|y| (x, y)).collect();
+        out.push(set);
+    }
+
+    // columns:
+    for y in 1..=9 {
+        let set = (1..=9).map(|x| (x, y)).collect();
+        out.push(set);
+    }
+
+    // boxes:
+    for x_ in [1, 4, 7] {
+        for y_ in [1, 4, 7] {
+            let mut inner = BTreeSet::new();
+            for x in 0..3 {
+                for y in 0..3 {
+                    inner.insert((x_+x, y_+y));
+                }
+            }
+            out.push(inner);
+        }
+    }
+
+    out
+}
+
 fn base_clauseset(smap: &mut SymbolMap) -> ClauseSet {
-    // for each (x, y)-cell:
-    // - a big disjunction saying it should have at least one value
-    // - and pairwise negated disjunctions to express it shouldn't have two
+    let mut c = ClauseSet::new();
 
-    // for each value n, and each cell set S:
-    // - a big disjunction saying S should have at least one n
-    // - and pairwise negated disjunctions to express it shouldn't have two
+    // each cell contains exactly one value.
+    for x in 1..=9 {
+        for y in 1..=9 {
+            let vars = (1..=9).map(|n| var(x, y, n, smap)).collect();
+            push_one_of(vars, smap, &mut c);
+        }
+    }
 
-    todo!()
+    // each index set contains exactly one n.
+    for n in 1..=9 {
+        for set in index_sets() {
+            let vars = set.iter().map(|(x, y)| var(*x, *y, n, smap)).collect();
+            push_one_of(vars, smap, &mut c);
+        }
+    }
+
+    c
 }
 
 // x, y, val in [1, 9]
