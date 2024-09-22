99fd1a032c7a35d26b6d3d69c06976dd35d45260
memoryleak47 <memoryleak47@protonmail.ch>
2024-09-22 18:44:36 +0200

    start backtrack mpl

--- src/cdcl/backtrack.rs
+++ src/cdcl/backtrack.rs
@@ -3,6 +3,31 @@ use crate::*;
 impl Cdcl {
     // returns whether we should continue. false means to return Outcome::Unsat.
     pub fn backtrack(&mut self, c: &Clause) -> bool {
+        let mut open: BTreeSet<_> = c.iter().map(|x| negate_literal(*x)).collect();
+
+        loop {
+            let Some((v, (b, cause))) = self.cause_stack.pop() else { return false; };
+
+            // only care for literals that are relevant for this contradiction.
+            if open.contains(&(v, b)) {
+                match cause {
+                    Cause::Branch => {
+                        self.unsimplify();
+                        self.apply_decision((v, !b), Cause::Lem);
+                        self.satisfied.push(open);
+                        return true;
+                    },
+                    Cause::Unit(clause) => {
+                        todo!()
+                    },
+                    _ => {},
+                }
+            }
+        }
+    }
+
+    // re-open clauses that were closed in a previous branch.
+    fn unsimplify(&mut self) {
         todo!()
     }
 }
