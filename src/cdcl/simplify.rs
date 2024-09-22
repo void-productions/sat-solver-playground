c7c34843df9a93628e68ec1707b6c17e1e4174c7
memoryleak47 <memoryleak47@protonmail.ch>
2024-09-22 17:19:21 +0200

    +cdcl::apply_decision

--- src/cdcl/simplify.rs
+++ src/cdcl/simplify.rs
@@ -4,4 +4,24 @@ impl Cdcl {
     pub fn simplify(&mut self) {
         todo!()
     }
+
+    // applies the decision both in the assignment, and the knowledge base.
+    pub fn apply_decision(&mut self, lit: Literal, cause: Cause) {
+        self.cause_stack.push(lit.0, (lit.1, cause));
+
+        let neg = negate_literal(lit);
+        self.open.retain_mut(|(x, y)| {
+            if x.contains(&lit) {
+                let combined = &*x | &*y;
+                self.satisfied.push(combined);
+                return false;
+            }
+
+            if x.contains(&neg) {
+                x.remove(&neg);
+            }
+
+            true
+        });
+    }
 }
