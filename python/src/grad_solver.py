26b3c2e6da5e99493097c3f85c629130bf2bb80d
Bluemi <bruno.schilling@protonmail.ch>
2024-09-22 20:19:23 +0200

    calc accuracy

--- python/src/grad_solver.py
+++ python/src/grad_solver.py
@@ -6,7 +6,7 @@ import numpy as np
 from knowledge_base import KnowledgeBase
 import torch
 
-from sudoku import Sudoku
+from sudoku import get_hard_solution
 
 
 def solve(knowledge_base: KnowledgeBase):
@@ -20,21 +20,32 @@ def solve(knowledge_base: KnowledgeBase):
     all_var_indices, all_positive = prepare_clause_sums(knowledge_base, device)
 
     learning_rate = 5.0
-    alpha = 0.002
+    alpha = 0.2
 
     optimizer = torch.optim.SGD([torch_variables], lr=learning_rate)
 
-    for epoch in range(3000):
+    for epoch in range(20):
         optimizer.zero_grad()
         torch_clause_sums = create_clause_sums(all_var_indices, all_positive, torch_variables)
-        satisfied_loss = get_satisfied_loss(torch_clause_sums)
+        satisfied_loss = get_satisfied_loss(torch_clause_sums) * 25
         decided_loss = get_decided_loss(torch_variables)
         loss = satisfied_loss + alpha * decided_loss
         loss.backward()
 
-        print(f'epoch={epoch:<4}  alpha={alpha:.3f}  Ls={satisfied_loss:.4f}  Ld={decided_loss:.4f}  L={loss:.4f}')
+        accuracy = calc_accuracy(torch_variables.detach().cpu().numpy(), knowledge_base, variables)
+
+        print(f'epoch={epoch:<4}  alpha={alpha:.3f}  Ls={satisfied_loss:.5f}  Ld={decided_loss:.5f}  L={loss:.4f}  '
+              f'acc={accuracy:.4f}')
 
         optimizer.step()
+
+        # print(torch_variables)
+
+        # alpha += 0.01
+        # if epoch % 100 == 0 and epoch > 0:
+        #     torch_variables = restart_variables(torch_variables)
+        #     optimizer = torch.optim.SGD([torch_variables], lr=learning_rate)
+        #     alpha = 0.01
 
     end_signs = torch.sign(torch_variables - 0.5)
 
@@ -42,16 +53,43 @@ def solve(knowledge_base: KnowledgeBase):
     lowest = np.argsort(result)
     sorted_variables = variables[lowest]
     sorted_probabilities = result[lowest]
-    sudoku = Sudoku()
+    sudoku = np.zeros((9, 9), dtype=np.int32)
+    real_sudoku = get_hard_solution()
     for variable, probability in zip(sorted_variables, sorted_probabilities):
         var_name = knowledge_base.mapping[variable]
-        print(f'{variable:<4} {probability:.3f} {var_name}')
+        x, y, value = int(var_name[1]), int(var_name[2]), int(var_name[3])
+        correct = int(real_sudoku[y - 1, x - 1] == value)
+        print(f'{variable:<4} {probability:.3f} {var_name}  correct={correct}')
         if probability > 0.5:
-            x, y, value = int(var_name[1]), int(var_name[2]), int(var_name[3])
-            sudoku.field[y-1, x-1] = value
-    print(sudoku.field)
+            sudoku[y-1, x-1] = value
+    print(sudoku)
     print('\nsame')
     print(torch.isclose(start_signs, end_signs).type(torch.int32))
+    accuracy = calc_accuracy(result, knowledge_base, variables)
+    print('accuracy:', accuracy)
+
+
+def calc_accuracy(result: np.ndarray, knowledge_base: KnowledgeBase, variables: np.ndarray):
+    lowest = np.argsort(result)
+    sorted_variables = variables[lowest]
+    sorted_probabilities = result[lowest]
+    sudoku = np.zeros((9, 9), dtype=np.int32)
+    for variable, probability in zip(sorted_variables, sorted_probabilities):
+        var_name = knowledge_base.mapping[variable]
+        if probability > 0.5:
+            x, y, value = int(var_name[1]), int(var_name[2]), int(var_name[3])
+            sudoku[y-1, x-1] = value
+
+    real_sudoku = get_hard_solution()
+
+    return np.sum(np.equal(sudoku, real_sudoku)) / np.sum(sudoku != 0)
+
+
+def restart_variables(variables):
+    new_variables = torch.sign(variables - 0.5) * 0.001 + 0.5
+    new_variables.detach_()
+    new_variables.requires_grad_(True)
+    return new_variables
 
 
 def create_variables(knowledge_base: KnowledgeBase, device: torch.device) -> Tuple[torch.Tensor, np.ndarray]:
@@ -95,4 +133,4 @@ def get_satisfied_loss(clause_sums: torch.Tensor) -> torch.Tensor:
 
 
 def get_decided_loss(variables) -> torch.Tensor:
-    return torch.mean(0.5 - torch.abs(variables - 0.5))
+    return torch.mean(torch.pow(0.5 - torch.abs(variables - 0.5), 3.0))
